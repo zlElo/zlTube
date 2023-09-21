e957ed104fe9372c7816c7a25f90b4af3beaa36c
zlElo <108819252+zlElo@users.noreply.github.com>
2023-09-21 09:08:06 +0200

    initial upload

---
+++ main.py
@@ -0,0 +1,211 @@
+import customtkinter
+from pytube import YouTube
+import threading
+from tkinter import filedialog
+import os
+from settings import settings_window
+import json
+import pyuac
+
+
+def video_meta_datas():
+    download_path = filedialog.askdirectory()
+
+    # Get the video URL from the entry widget
+    url = url_entry.get()
+
+    # Create a YouTube object
+    yt = YouTube(url)
+
+    # Get the video details
+    title_yt = yt.title
+    length_yt = yt.length
+
+    if len(title_yt) > 25:
+        title_yt = f'{title_yt[:25]}...'
+
+    # Print the video details
+    print("Title:", title_yt)
+    print("Length:", length_yt, seconds)
+
+    title_value = f'"{title_yt}"'
+    value_title.configure(text=title_value)
+
+    length_value = f'{length_yt} {seconds}'
+    value_length.configure(text=length_value)
+
+    # refresh window
+    window.update()
+    window.update_idletasks()
+
+
+    if file_format_box.get() == '.mp4':
+        download_thread = threading.Thread(target=lambda: download_as_mp4(yt, download_path))
+        download_thread.start()
+
+    elif file_format_box.get() == '.mp3':
+        download_thread = threading.Thread(target=lambda: download_as_mp3(yt, download_path))
+        download_thread.start()
+
+    
+
+
+def download_as_mp4(yt, download_path):
+    # progress title
+    label_done.configure(text=downloading, text_color='red')
+
+    # refresh window
+    window.update()
+    window.update_idletasks()
+    
+
+    # Download the video
+    stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    stream.download(download_path)
+
+    # done title
+    label_done.configure(text=saved, text_color='green')
+
+    # refresh window
+    window.update()
+    window.update_idletasks()
+
+    # console logging
+    print('[log] download done!')
+
+
+def download_as_mp3(yt, download_path):
+    # progress title
+    label_done.configure(text=downloading, text_color='red')
+
+    # refresh window
+    window.update()
+    window.update_idletasks()
+    
+
+    audio_streams = yt.streams.filter(only_audio=True)
+
+    # Wählen Sie den ersten Audio-Stream
+    audio = audio_streams.first()
+
+    # Herunterladen des Audio-Streams
+    audio.download(download_path)
+
+    # Konvertieren Sie das heruntergeladene Audio in MP3
+    base, ext = os.path.splitext(f'{download_path}/{audio.default_filename}')
+    new_file = f'{base}.mp3'
+    os.rename(f'{download_path}/{audio.default_filename}', new_file)
+    
+
+    # done title
+    label_done.configure(text=saved, text_color='green')
+
+    # refresh window
+    window.update()
+    window.update_idletasks()
+
+    # console logging
+    print('[log] download done!')
+
+
+def admin_prompt():
+    if pyuac.isUserAdmin():
+        settings_window()
+    else:
+        window.destroy()
+        print("[log] re-launching as admin")
+        pyuac.runAsAdmin() 
+        
+        
+
+
+
+# Create a Tkinter window
+window = customtkinter.CTk()
+window.title("zlTube")
+window.geometry('300x400')
+window.minsize(300, 400)
+window.maxsize(300, 400)
+window.iconbitmap("icon.ico")
+
+
+
+# get language package on start
+with open('settings.json', 'r') as f:
+    data = json.load(f) # load the JSON data from the file
+    language = data['language']
+
+# if german language is selected
+if language == 'Deutsch':
+    with open('languages/de.json', 'r') as f_de:
+        data_de = json.load(f_de) # load the JSON data from the file
+        length = data_de['length']
+        title = data_de['title']
+        seconds = data_de['seconds']
+        downloading = data_de['downloading']
+        saved = data_de['saved']
+        download = data_de['download']
+        progress = data_de['progress']
+
+# if german language is selected
+if language == 'English':
+    with open('languages/en.json', 'r') as f_en:
+        data_en = json.load(f_en) # load the JSON data from the file
+        length = data_en['length']
+        title = data_en['title']
+        seconds = data_en['seconds']
+        downloading = data_en['downloading']
+        saved = data_en['saved']
+        download = data_en['download']
+        progress = data_en['progress']
+
+if pyuac.isUserAdmin():
+    settings_window()
+
+# main GUI
+settings_button = customtkinter.CTkButton(window, text='⚙', width=10, command=admin_prompt)
+settings_button.place(x=260, y=10)
+
+# Add a label to prompt the user to enter the video URL
+url_label = customtkinter.CTkLabel(window, text='Video URL:')
+url_label.pack()
+
+# Add an entry widget to allow the user to enter the video URL
+url_entry = customtkinter.CTkEntry(window)
+url_entry.pack()
+
+file_format_box = customtkinter.CTkComboBox(window, values=['.mp4', '.mp3'])
+file_format_box.pack(pady=10)
+
+# Add a button to initiate the download process
+download_button = customtkinter.CTkButton(window, text=download, command=video_meta_datas)
+download_button.pack(pady=15)
+
+backround_frame = customtkinter.CTkFrame(window)
+backround_frame.pack(pady=30)
+
+
+title_info_vid = customtkinter.CTkLabel(backround_frame, text=title)
+title_info_vid.pack(padx=80, pady=3)
+
+value_title = customtkinter.CTkLabel(backround_frame, text='', text_color='green')
+value_title.pack()
+
+
+length_info_vid = customtkinter.CTkLabel(backround_frame, text=length)
+length_info_vid.pack(padx=80, pady=3)
+
+value_length = customtkinter.CTkLabel(backround_frame, text='', text_color='green')
+value_length.pack()
+
+
+label_done_ = customtkinter.CTkLabel(backround_frame, text=progress)
+label_done_.pack(padx=80, pady=3)
+
+label_done = customtkinter.CTkLabel(backround_frame, text='', text_color='green')
+label_done.pack()
+
+
+
+# Display the window
+window.mainloop()
---
+++ settings.py
@@ -0,0 +1,43 @@
+import customtkinter
+import json
+
+
+def settings_window():
+    
+    # this is the function called when the button is clicked
+    def save_settings():
+        language = language_combo.get() # get the selected language from the combobox
+        with open('settings.json', 'r+') as f:
+            data = json.load(f) # load the JSON data from the file
+            data['language'] = language # add the new language entry to the data
+            f.seek(0) # move the file pointer to the beginning of the file
+            json.dump(data, f, indent=4) # write the updated data back to the file with indentation
+        status.configure(text='✅')
+        
+
+    root = customtkinter.CTk()
+    
+
+    # This is the section of code which creates the main window
+    root.geometry('250x150')
+    root.title('settings')
+    root.iconbitmap("icon.ico")
+
+
+    # This is the section of code which creates the a label
+    customtkinter.CTkLabel(root, text='Language:').pack(pady=10)
+
+
+    # This is the section of code which creates a combo box
+    language_combo= customtkinter.CTkComboBox(root, values=['English', 'Deutsch'])
+    language_combo.pack()
+
+
+    # This is the section of code which creates a button
+    customtkinter.CTkButton(root, text='save', command=save_settings).pack(pady=20)
+
+    status = customtkinter.CTkLabel(root, text='')
+    status.place(x=3, y=125)
+
+
+    root.mainloop()
